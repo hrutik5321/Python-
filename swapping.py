d60a3f127e5b81aa5394c78324ccf9e35cbfa46c
dell <hrutikdhumal2003@gmail.com>
2019-11-29 14:07:27 -0800

    proper code

---
+++ swapping.py
@@ -0,0 +1,11 @@
+
+a = 5
+b = 7
+
+temp = a  # there is another method without using third variable this work only in python
+a = b     # a,b = b,a
+b = temp  # we solve first right side hence the value of b is stored in a an value of b is stored in a
+
+
+print(a)
+print(b)
